d9a75dbfeb068866253cdc842faacb5203aea28f
Edward <tuananhdo.disney01@gmail.com>
2023-04-17 15:50:35 +0700

    changes wallet

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -21,7 +21,6 @@ export enum ToastifyStatus {
 }
 
 const Header = ({}: HeaderProps) => {
-  const { ethereum }: any = window;
   const [provider, setProvider] = useState(undefined);
   const [signer, setSigner] = useState(undefined);
   const [signerAddress, setSignerAddress] = useState<any>(undefined);
@@ -42,7 +41,7 @@ const Header = ({}: HeaderProps) => {
 
   useEffect(() => {
     onLoad();
-  }, [ethereum]);
+  }, []);
 
   const onLoad = async () => {
     const { ethereum }: any = window;
@@ -81,7 +80,6 @@ const Header = ({}: HeaderProps) => {
   const handleAccountsChanged = (accounts: any) => {
     setSignerAddress(accounts);
     setAddedWallet(accounts);
-    getWalletAddress(signer, uniContract);
   };
 
   const isWalletConnected = () => signer !== undefined;
@@ -117,7 +115,7 @@ const Header = ({}: HeaderProps) => {
       getWalletAddress(signer, uniContract);
       setIsConnected(true);
     }
-  }, [signer, uniContract, isConnected]);
+  }, [signer, uniContract, isConnected, signerAddress]);
 
   const displayAddress =
     signerAddress !== undefined
@@ -182,7 +180,7 @@ const Header = ({}: HeaderProps) => {
         </div>
         <Button
           style={styles.balance}
-          onClick={() => console.log(123)}
+          onClick={() => getWalletAddress(signer, uniContract)}
           title={
             <div>{`${
               !uniAmount ? "Loading..." : Number(uniAmount).toFixed(1)
