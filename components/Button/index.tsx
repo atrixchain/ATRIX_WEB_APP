d75b9023dc0268abc2e5a35909d68f4fd20415bf
Edward <tpazyot127@gmail.com>
2024-03-15 14:18:22 +0700

    updated

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,12 +2,12 @@ import { Button } from "antd";
 
 type ButtonProps = {
   title: JSX.Element;
-  type?: "link" | "text" | "ghost" | "default" | "primary" | "dashed";
+  type?: "link" | "text" | "default" | "primary" | "dashed";
   style: string;
   onClick: (evt: any) => void;
 };
 
-const cardButton = ({ title, type, style, onClick }: ButtonProps) => {
+const CardButton = ({ title, type, style, onClick }: ButtonProps) => {
   return (
     <Button onClick={onClick} type={type} className={style}>
       {title}
@@ -15,4 +15,4 @@ const cardButton = ({ title, type, style, onClick }: ButtonProps) => {
   );
 };
 
-export default cardButton;
+export default CardButton;
